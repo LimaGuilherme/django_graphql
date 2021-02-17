d3ac0348d39f5417f8316d5790e276d1c1603e5a
guilherme <guilherme@inceres.com.br>
2021-02-17 15:00:16 -0300

    adding list schema to cards and decks.

--- apps/schema/schema.py
+++ apps/schema/schema.py
@@ -3,6 +3,8 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from apps.users.models import User as UserModel
+from apps.decks.models import Deck as DeckModel
+from apps.cards.models import Card as CardModel
 
 
 class User(DjangoObjectType):
@@ -10,11 +12,26 @@ class User(DjangoObjectType):
         model = UserModel
 
 
+class Deck(DjangoObjectType):
+    class Meta:
+        model = DeckModel
+
+
+class Card(DjangoObjectType):
+    class Meta:
+        model = CardModel
+
+
 class Query(graphene.ObjectType):
     users = graphene.List(User)
+    decks = graphene.List(Deck)
+    cards = graphene.List(Card)
 
     def resolve_users(self, info):
         return UserModel.objects.all()
+
+    def resolve_decks(self, info):
+        return DeckModel.objects.all()
 
 
 schema = graphene.Schema(query=Query)
