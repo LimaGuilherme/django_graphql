6f4c57a490a69b30ef9018881a530c0640c59ac3
guilherme <guilherme@inceres.com.br>
2021-02-23 10:42:28 -0300

    update card support

--- apps/schema/schema.py
+++ apps/schema/schema.py
@@ -1,15 +1,20 @@
 import graphene
 
+from datetime import timedelta
+from django.utils import timezone
+
 from graphene_django import DjangoObjectType
 
-from apps.users.models import User as UserModel
 from apps.decks.models import Deck as DeckModel
 from apps.cards.models import Card as CardModel
 
-
-class User(DjangoObjectType):
-    class Meta:
-        model = UserModel
+buckets = (
+    (1, 1),
+    (2, 3),
+    (3, 7),
+    (4, 1),
+    (5, 3)
+)
 
 
 class Deck(DjangoObjectType):
@@ -51,25 +56,65 @@ class CreateCard(graphene.Mutation):
         return CreateCard(card=card)
 
 
+class UpdateCard(graphene.Mutation):
+    card = graphene.Field(Card)
+
+    class Arguments:
+        id = graphene.ID()
+        question = graphene.String()
+        answer = graphene.String()
+        status = graphene.Int()
+
+    def mutate(self, info, id, question, answer, status):
+        card = CardModel.objects.get(pk=id)
+        bucket = card.bucket
+
+        if status == 1 and bucket > 1:
+            bucket -= 1
+        elif status == 3 and bucket <= 4:
+            bucket += 1
+
+        days = buckets[bucket-1][1]
+        next_review_at = timezone.now() + timedelta(days=days)
+
+        card.question = question
+        card.answer = answer
+        card.bucket = bucket
+        card.next_review_at = next_review_at
+        card.last_review_at = timezone.now()
+        card.save()
+        return UpdateCard(card=card)
+
+
 class Mutation(graphene.ObjectType):
     create_card = CreateCard.Field()
     create_deck = CreateDeck.Field()
 
+    update_card = UpdateCard.Field()
+
 
 class Query(graphene.ObjectType):
-    users = graphene.List(User)
     decks = graphene.List(Deck)
-    cards = graphene.List(Card)
-    deck_cards = graphene.List(Card)
+    deck_by_id = graphene.List(Deck, id=graphene.Int())
+    deck_cards = graphene.List(Card, deck=graphene.Int())
 
-    def resolve_users(self, info):
-        return UserModel.objects.all()
+    cards = graphene.List(Card)
+    card_by_id = graphene.List(Card, id=graphene.Int())
 
     def resolve_decks(self, info):
         return DeckModel.objects.all()
 
     def resolve_cards(self, info):
         return CardModel.objects.all()
+
+    def resolve_deck_cards(self, info, deck):
+        return CardModel.objects.filter(deck=deck)
+
+    def resolve_deck_by_id(self, info, id):
+        return DeckModel.objects.filter(pk=id)
+
+    def resolve_card_by_id(self, info, id):
+        return CardModel.objects.filter(pk=id)
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
